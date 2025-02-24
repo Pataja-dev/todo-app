2af493f54d156551398f7dab9b84f21d3ba41799
JL <johnlloydglee26@gmail.com>
2025-02-24 14:11:24 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,26 @@ const Home = () => {
     setTasks([...tasks, task]);
     setTask('');
   };
-
   const removeTask = (taskToRemove: string) => {
     setTasks(tasks.filter(t => t !== taskToRemove));
   };
-
   useEffect(() => {
     console.log('Tasks updated:', tasks);
   }, [tasks]);
 
   return (
-    <div className='bg-stone-400'>
-      <form className="max-w-sm mx-auto flex">
+    <div className=''>
+      <div className="grid-cols-6">
+      <form className="max-w-md mx-auto">
         <textarea
           value={task}
           onChange={(e) => setTask(e.target.value)}
-          className="block p-2.5 w-full text-sm mt-10 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+          className="block p-2.5 w-full text-sm mt-10 text-gray-900 bg-stone-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:bg-stone-200" 
           placeholder="Enter a Task..."
         />
+    <button onClick={addTask} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Task</button>
     </form>
-    <button onClick={addTask} type="button" className="align-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Task</button>
+      </div >
       <TaskList tasks={tasks} removeTask={removeTask} />
     </div>
   );
