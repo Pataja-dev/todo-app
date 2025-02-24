a66ac9cbfe1dc7ef0aec9d6e153d627f94a5612a
JL <johnlloydglee26@gmail.com>
2025-02-24 16:57:27 +0800

    final

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -10,13 +10,14 @@ const TaskList: React.FC<TaskListProps> = ({ tasks = [], removeTask }) => {
   return (
     <>
     
-        <div className="bg-stone-400 relative overflow-x-auto shadow-md sm:rounded-lg max-w-md mx-auto">
+        <div className=" relative overflow-x-auto shadow-md sm:rounded-lg max-w-2xl mx-auto">
+            <h1 className='text-center'>To-Do</h1>
         <ul className="list-none">  
         {tasks.map((task, index) => (
           <li //- Display all taskss in a list format.
             key={index} 
-            className="flex justify-between items-center odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200 px-6 py-4">
-                <span className="font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            className="flex justify-between items-center odd:bg-white border-b border-gray-200 px-6 py-4">
+                <span className="font-medium text-gray-900 whitespace-nowrap">
                 {task}
                 </span>
                 <button  //- Add a "Remove" button next to each task to delete it from the list.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,27 @@
 
 "use client"
 import { useState, useEffect } from 'react';
-import TaskList from '@/app/components/TaskList/page';
+import TaskList from '@/app/components/TaskList';
 
 const Home = () => {
   const [task, setTask] = useState<string>('');
   const [tasks, setTasks] = useState<string[]>([]);
+  const [isClick, setIsClick] = useState<boolean>(false);
 
   const addTask = () => {
     if (task.trim() === '') return;
     setTasks([...tasks, task]);
     setTask(''); //     - Clear the input field after adding the task.
+    setIsClick(true);
   };
-  const removeTask = (taskToRemove: string) => {
-    setTasks(tasks.filter(t => t !== taskToRemove));
-  };
+    const removeTask = (taskToRemove: string) => {
+      const updatedTasks = tasks.filter(t => t !== taskToRemove);
+      setTasks(updatedTasks);
+      if (updatedTasks.length === 0) {
+        setIsClick(false);
+    }
+      
+    };
 
   
   useEffect(() => { // - Use the useEffect hook to log a message to the console whenever the list of tasks changes.
@@ -29,7 +36,7 @@ const Home = () => {
   return (
     <div className='pt-52'>
       <div className="grid-cols-6 mb-10">
-      <form className="max-w-md mx-auto flex">
+      <form className="max-w-2xl mx-auto flex">
         <input //  - Create an input field for users to type a task.
           value={task}
           onChange={(e) => setTask(e.target.value)}
@@ -43,12 +50,13 @@ const Home = () => {
         className="sm:h-[60px] h-60 mt-10  sm:mt-10 ml-4 sm:ml-4 w-[100px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center">
           Add Task
       </button>
+
     </form>
       </div >
-      <TaskList 
+      {isClick && <TaskList //added some conditional rendering
       tasks={tasks} //- Pass the list of tasks as a prop from the parent component to the child component.
       removeTask={removeTask} //- Pass the state and any necessary functions (e.g., removeTask) from the parent component to the child component as props.
-      />  
+      />  }
     </div>
   );
 };
